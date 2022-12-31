74251012e6e81b4771c5293c81a2258dc5461d65
Febrian SF <febriansf@student.uns.ac.id>
2022-12-31 12:55:41 +0700

    Define attribute komposisi, beratPerPcs Add constructor to initialize value of jenisRoti

--- src/roti/Manis.java
+++ src/roti/Manis.java
@@ -9,6 +9,26 @@ package roti;
  * @author BrxxN
  */
 public class Manis extends Roti {
+    
+    // Komposisi roti manis
+    private final int[] komposisi = {
+        1000, //0.Tepung Terigu
+        150,  //1.Gula Pasir
+        150,  //2.Butter
+        20,   //3.Ragi
+        300,  //4.Susu Bubuk
+        250,  //5.Susu Cair
+        80,   //6.Telur
+        400   //7.Es Batu
+    };
+    
+    // Berat dough per pcs
+    private final int beratPerPcs = 50;    
+
+    // Constructor
+    public Manis() {
+        this.jenisRoti = "Roti Manis";
+    }
 
     @Override
     public void pilihVarian(int nomorVarian) {
