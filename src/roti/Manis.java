3d0dd16e74434cf98cb21b6618dd64f7a7489b34
Febrian SF <febriansf@student.uns.ac.id>
2022-12-31 12:57:39 +0700

    Create method hitungTotalBahan

--- src/roti/Manis.java
+++ src/roti/Manis.java
@@ -4,6 +4,8 @@
  */
 package roti;
 
+import java.util.stream.IntStream;
+
 /**
  *
  * @author BrxxN
@@ -38,7 +40,21 @@ public class Manis extends Roti {
     protected void hitungVarian() {
     }
 
+    // Method untuk menghitung BERAT per bahan yang diperlukan berdasarkan banyaknya pesanan dan komposisi
     @Override
     protected void hitungTotalBahan() {
+        double beratDough = IntStream.of(this.komposisi).sum();
+        
+        double beratTotal = this.beratPerPcs * this.jumPesanan;
+        
+        this.totalDough = beratTotal;
+        this.tepungTerigu = beratTotal / beratDough * this.komposisi[0];
+        this.gulaPasir = beratTotal / beratDough * this.komposisi[1];
+        this.butter = beratTotal / beratDough * this.komposisi[2];
+        this.ragi = beratTotal / beratDough * this.komposisi[3];
+        this.susuBubuk = beratTotal / beratDough * this.komposisi[4];
+        this.susuCair = beratTotal / beratDough * this.komposisi[5];
+        this.telur = beratTotal / beratDough * this.komposisi[6];
+        this.esBatu = beratTotal / beratDough * this.komposisi[7];
     }
 }
