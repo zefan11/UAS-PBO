84ef210a2f3e428a88921bcee03422778ce6a007
Febrian SF <febriansf@student.uns.ac.id>
2022-12-31 12:46:54 +0700

    Define attribute in class Roti

--- src/roti/Roti.java
+++ src/roti/Roti.java
@@ -10,4 +10,33 @@ package roti;
  */
 public class Roti {
     
+    // Attribute untuk menyimpan nama Jenis Roti
+    protected String jenisRoti;
+    
+    // Bahan Yang diperlukan
+    protected double tepungTerigu;
+    protected double gulaPasir;
+    protected double butter;
+    protected double ragi;
+    protected double susuBubuk;
+    protected double susuCair;
+    protected double telur;
+    protected double esBatu;
+    protected double totalDough;
+    
+    // Jumlah Pesananan
+    protected int jumPesanan;
+    
+    // Banyaknya Beli Kemasan per Bahan
+    protected double modalBahan[] = new double[8];
+    
+    // Harga Beli Total Kemasan per Bahan
+    protected double modalHarga[] = new double[8];
+    
+    // Total modal yang diperlukan
+    protected double totalModal;
+    
+    // Attribute untuk menyimpan pilihan varian
+    protected int nomorVarian;
+
 }
